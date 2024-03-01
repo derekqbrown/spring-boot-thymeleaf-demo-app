b422baa519938296d0d47c415d36897861491b54
Derek Brown <72898682+derekqbrown@users.noreply.github.com>
2024-02-29 20:33:56 -0700

    Update LeaveRequest.java
    Removed unnecessary commented code

--- SpringBootThymeleafEmployeeApp/src/main/java/com/example/model/LeaveRequest.java
+++ SpringBootThymeleafEmployeeApp/src/main/java/com/example/model/LeaveRequest.java
@@ -34,42 +34,4 @@ public class LeaveRequest {
 	@Column(name="`status`")
 	private String status;
 	
-//	public int getRequestId() {
-//		return requestId;
-//	}
-//	public void setRequestId(int requestId) {
-//		this.requestId = requestId;
-//	}
-//	public int getEmployeeId() {
-//		return employeeId;
-//	}
-//	public void setEmployeeId(int employeeId) {
-//		this.employeeId = employeeId;
-//	}
-//	public String getReason() {
-//		return reason;
-//	}
-//	public void setReason(String reason) {
-//		this.reason = reason;
-//	}
-//	public Date getStart() {
-//		return start;
-//	}
-//	public void setStart(Date start) {
-//		this.start = start;
-//	}
-//	public Date getEnd() {
-//		return end;
-//	}
-//	public void setEnd(Date end) {
-//		this.end = end;
-//	}
-//	public String getStatus() {
-//		return status;
-//	}
-//	public void setStatus(String status) {
-//		this.status = status;
-//	}
-	
-	
 }
